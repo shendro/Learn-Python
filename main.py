505ab4ac5f183fa01e191530d166aba01fba48d7
shendro <96789957+shendro@users.noreply.github.com>
2022-07-22 19:09:51 +0300

    Excercise L3

---
+++ main.py
@@ -0,0 +1,15 @@
+#variables
+place = "Hawaii"
+writer = "Writer"
+person = ["Name1", "Name2", "Name3", "Name4", "Name5", "Name6"]
+age = 33
+text = ["I thank you for your friendly letters. You sure know how to cheer up a really old geezer", """ in his sunset years. I don’t make public appearances any more because I now resemble nothing so much as an iguana.\n
+What I had to say to you, moreover, would not take long, to wit: Practice any art, music, singing, dancing, acting, drawing, painting, sculpting, poetry, fiction, essays, reportage, no matter how well or badly, not to get money and fame, but to experience becoming, to find out what’s inside you, to make your soul grow.\n
+Seriously! I mean starting right now, do art and do it for the rest of your lives. Draw a funny or nice picture of Ms. Lockwood, and give it to her. Dance home after school, and sing in the shower and on and on. Make a face in your mashed potatoes. Pretend you’re Count Dracula.\n
+Here’s an assignment for tonight, and I hope Ms. Lockwood will flunk you if you don’t do it: Write a six line poem, about anything, but rhymed. No fair tennis without a net. Make it as good as you possibly can. But don’t tell anybody what you’re doing. Don’t show it or recite it to anybody, not even your girlfriend or parents or whatever, or Ms. Lockwood. OK?\n
+Tear it up into teeny-weeny pieces, and discard them into widely separated trash recepticals. You will find that you have already been gloriously rewarded for your poem. You have experienced becoming, learned a lot more about what’s inside you, and you have made your soul grow.\n
+God bless you all!\n"""]
+
+#testing code
+print(f"Dear {place}, and {person[0]}, and {person[1]}, and {person[2]}, {person[3]}, {person[4]} and {person[5]}:\n"
+      f"{text[0]} {age}{text[1]}{writer}")
